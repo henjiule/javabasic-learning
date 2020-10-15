698e9a4d013dfc5252a0ba7251c68abd17e5acdc
zhangcong <1041526741@qq.com>
2020-10-15 17:02:15 +0800

    Example2_1

---
+++ java-basic/src/main/java/com/congzhang/java/README/Week2/Example2_1.java
@@ -0,0 +1,18 @@
+package com.congzhang.java.README.Week2;
+
+/**
+ * @ClassName Example2_1
+ * @Description TODO
+ * @Author zhangcong
+ * @Date 2020/10/15
+ **/
+public class Example2_1 {
+    public static void main (String args[ ]){
+        char ch1='w',ch2='好';
+        int p1=32831,p2=30452;
+        System.out.println("\""+ch1+"\"的位置:"+(int)ch1);
+        System.out.println("\""+ch2+"\"的位置:"+(int)ch2);
+        System.out.println("第"+p1+"个位置上的字符:"+(char)p1);
+        System.out.println("第"+p2+"个位置上的字符:"+(char)p2);
+    }
+}
