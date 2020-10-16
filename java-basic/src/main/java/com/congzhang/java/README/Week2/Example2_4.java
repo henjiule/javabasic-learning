027042cb0f5ee591a224c39a0ad24f3c7629723f
zhangcong <1041526741@qq.com>
2020-10-16 12:08:43 +0800

    Example2_4

---
+++ java-basic/src/main/java/com/congzhang/java/README/Week2/Example2_4.java
@@ -0,0 +1,25 @@
+package com.congzhang.java.README.Week2;
+
+/**
+ * @ClassName Example2_4
+ * @Description TODO
+ * @Author zhangcong
+ * @Date 2020/10/16
+ **/
+public class Example2_4 {
+    public static void main(String args[]){
+        int a[]={1,2,3,4};
+        int b[]={100,200,300};
+        System.out.println("数组a的元素个数="+a.length);
+        System.out.println("数组b的元素个数="+b.length);
+        System.out.println("数组a的引用="+a);
+        System.out.println("数组b的引用="+b);
+        System.out.println("a==b的结果是"+(a==b));
+        a=b;
+        System.out.println("数组a的元素个数="+a.length);
+        System.out.println("数组b的元素个数="+b.length);
+        System.out.println("a==b的结果是"+(a==b));
+        System.out.println("a[0]="+a[0]+",a[1]="+a[1]+",a[2]="+a[2]);
+        System.out.print("b[0]="+a[0]+",b[1]="+b[1]+",b[2]="+b[2]);
+    }
+}
