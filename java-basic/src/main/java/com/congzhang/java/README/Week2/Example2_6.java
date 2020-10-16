a13d12b3a80110a7dd096ca17e717cdb73cd2267
zhangcong <1041526741@qq.com>
2020-10-16 12:18:36 +0800

    Example2_6

---
+++ java-basic/src/main/java/com/congzhang/java/README/Week2/Example2_6.java
@@ -0,0 +1,25 @@
+package com.congzhang.java.README.Week2;
+
+/**
+ * @ClassName Example2_6
+ * @Description TODO
+ * @Author zhangcong
+ * @Date 2020/10/16
+ **/
+import java.util.*;
+public class Example2_6 {
+    public static void main(String args[]){
+        int [] a ={10,20,30,40,50,60},b,c,d;
+        b=Arrays.copyOf(a,10);
+        System.out.println("数组a的各个元素中的值:");
+        System.out.println(Arrays.toString(a));
+        System.out.println("数组b的各个元素中的值ֵ:");
+        System.out.println(Arrays.toString(b));
+        c=Arrays.copyOfRange(a,3,5);
+        System.out.println("数组c的各个元素中的值:");
+        System.out.println(Arrays.toString(c));
+        d=Arrays.copyOfRange(a,3,9);
+        System.out.println("数组d的各个元素中的值:");
+        System.out.println(Arrays.toString(d));
+    }
+}
