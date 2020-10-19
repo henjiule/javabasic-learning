ce4b023c03e4253edbea3d9f0dad89cf11e831e6
zhangcong <1041526741@qq.com>
2020-10-19 21:17:00 +0800

    Example3_1

---
+++ java-basic/src/main/java/com/congzhang/java/README/Week3/Example3_1.java
@@ -0,0 +1,24 @@
+package com.congzhang.java.README.Week3;
+
+/**
+ * @ClassName Example3_1
+ * @Description TODO
+ * @Author zhangcong
+ * @Date 2020/10/19
+ **/
+public class Example3_1 {
+    public static void main(String args[]) {
+        char a1='中',a2='国',a3='科',a4='大';
+        char secret='A';
+        a1=(char)(a1^secret);
+        a2=(char)(a2^secret);
+        a3=(char)(a3^secret);
+        a4=(char)(a4^secret);
+        System.out.println("密文:"+a1+a2+a3+a4);
+        a1=(char)(a1^secret);
+        a2=(char)(a2^secret);
+        a3=(char)(a3^secret);
+        a4=(char)(a4^secret);
+        System.out.println("原文:"+a1+a2+a3+a4);
+    }
+}
