522c9a29ac8afc83b43c4b10ecef3c13f34c48dc
zhangcong <1041526741@qq.com>
2020-10-22 15:58:22 +0800

    Example4_1

---
+++ java-basic/src/main/java/com/congzhang/java/README/Week3/Example4_1.java
@@ -0,0 +1,22 @@
+package com.congzhang.java.README.Week3;
+
+/**
+ * @ClassName Example4_1
+ * @Description TODO
+ * @Author zhangcong
+ * @Date 2020/10/22
+ **/
+class XiyoujiRenwu {
+    float height,weight;
+    String head, ear,hand,foot, mouth;
+    void speak(String s) {
+        System.out.println(s);
+    }
+}
+public class Example4_1 {
+    public static void main(String args[]) {
+        XiyoujiRenwu  zhubajie,sunwukong;
+        zhubajie=new  XiyoujiRenwu();
+        sunwukong=new  XiyoujiRenwu();
+    }
+}
